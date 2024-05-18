5647144b3420a74788b08b83a4aafa6473afa862
DimaHusen <komuk11107@gmail.com>
2024-05-18 14:31:31 +0300

    И удалил ненужный тест

--- src/Services/StudentService/Tests/EduHub.StudentService.Tests.Unit/Tests/Courses/CourseNegativeTests.cs
+++ src/Services/StudentService/Tests/EduHub.StudentService.Tests.Unit/Tests/Courses/CourseNegativeTests.cs
@@ -23,25 +23,5 @@ namespace Unit.Tests.Courses
             // Assert: Проверка на исключение ArgumentException
             course.Should().Throw<ArgumentException>();
         }
-        
-        [Theory]
-        [MemberData(nameof(CourseProperties))]
-        public void UpdateCourse_WithNullData_ShouldBeInvalid(
-            Guid id,
-            string name,
-            string description,
-            Guid educatorId)
-        {
-            // Arrange: Подготовка данных
-            // Данные передаются через параметризованный тест, данные уже подготовлены
-            // Создание экземпляра Course с валидными данными
-            var course = new Course(id, name, description, educatorId);
-
-            // Act: Лямбда-выражение для обновления курса с нулевыми данными и проверки на исключение
-            var updateCourse = () => course.Update(name, description, educatorId);
-
-            // Assert: Проверка на исключение ArgumentException
-            updateCourse.Should().Throw<ArgumentException>();
-        }
     }
 }
